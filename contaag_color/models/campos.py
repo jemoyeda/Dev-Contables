dafeb508705446ff1a4f6fd17ca30b201fe348ce
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-05-25 13:44:40 -0400

    color_11

---
+++ contaag_color/models/campos.py
@@ -0,0 +1,7 @@
+from odoo import models, fields 
+
+#creando campos del modulo
+class producto(models.Model):
+    _inherit = 'product.template'
+
+    color_id = fields.Many2one(comodel_name='colores', string='Color')
