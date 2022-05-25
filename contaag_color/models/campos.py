a72fc42878d2e9213029e36d0ae507fca3e02cd5
Ing. Hermes Colina <79210037+HermesColina2909@users.noreply.github.com>
2022-05-25 13:43:31 -0400

    Delete contaag_color directory

--- contaag_color/models/campos.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields 
-
-#creando campos del modulo
-class producto(models.Model):
-    _inherit = 'product.template'
-
-    color_id = fields.Many2one(comodel_name='colores', string='Color')
